1cf5e9354906d0645efda463e600f94eccd14dd0
hiraberu <s3nagai@gmail.com>
2020-06-29 02:29:59 +0900

    ・詳細説明と補足説明の表示/非表示を付けた。 ・スプレッドシートで色付けているところを、手動でclass設定した(GASで色取得は重かった)

---
+++ showcolumn.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+var array = ["detail","supplement"];
+    for(var j=0;j<array.length;j++){
+        var id = array[j] + "_display";
+        var obj = array[j] + "_check";
+        var CELL = document.getElementById(id);
+        var TABLE = CELL.parentNode.parentNode.parentNode;
+        for(var i=0;TABLE.rows[i];i++) {
+            TABLE.rows[i].cells[CELL.cellIndex].style.display = (document.getElementById(obj).checked) ? '' : 'none';
+        }
+    }
+}
+
+function checkbox_cell( obj,id ){
+    var CELL = document.getElementById(id);
+    var TABLE = CELL.parentNode.parentNode.parentNode;
+    for(var i=0;TABLE.rows[i];i++) {
+        TABLE.rows[i].cells[CELL.cellIndex].style.display = (obj.checked) ? '' : 'none';
+    }
+}
+
+/*
+// class–¼‚ÅŒ©‚é‚Æ‚«
+function checkbox_cell( obj,id ) {
+  var element = document.getElementsByClassName(id);
+  for( var i = 0; i < element.length; i++ ){
+    element[i].style.display = obj.checked ? '' : 'none';
+  }
+}
+*/
