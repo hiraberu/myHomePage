f63ec3631d8e91f014fa16a20884076b82a420f7
hiraberu <s3nagai@gmail.com>
2020-07-22 18:04:35 +0900

    iframe自動調整テスト

---
+++ nejire/adjust_frame_css.js
@@ -0,0 +1,13 @@
+function adjust_frame_css(F){
+  if(document.getElementById(F)) {
+    var myF = document.getElementById(F);
+    var myC = myF.contentWindow.document.documentElement;
+    var myH = 500;
+    if(document.all) {
+      myH  = myC.scrollHeight;
+    } else {
+      myH = myC.offsetHeight;
+    }
+    myF.style.height = myH+"px";
+  }
+}
