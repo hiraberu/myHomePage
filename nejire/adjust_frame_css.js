b500cc213bfc7890bb2b7bfda557f540c252d192
hiraberu <s3nagai@gmail.com>
2020-07-25 03:20:20 +0900

    左右分けて、別々にスクロールできるようにした

--- nejire/adjust_frame_css.js
+++ nejire/adjust_frame_css.js
@@ -1,3 +1,4 @@
+/* フレーム内に読み込むドキュメントサイズによって高さを変更(下のを使うことにしたので未使用) */
 function adjust_frame_css(F){
   if(document.getElementById(F)) {
     var myF = document.getElementById(F);
@@ -10,4 +11,13 @@ function adjust_frame_css(F){
     }
     myF.style.height = myH+"px";
   }
+}
+
+/* ウィンドウのサイズに合わせて、説明表示領域の高さを変更 */
+
+window.onresize = change_height_css;
+
+function change_height_css(){
+  var myFrame = document.getElementById( "descframe" );
+  myFrame.style.height = (document.documentElement.clientHeight-12)+"px";
 }
